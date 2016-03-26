c175a12fbea1077da38cb1d4cd265627fb8ba71c
Mihir Patil <mihirmpatil@gmail.com>
2016-03-25 23:20:40 -0500

    invert binary tree

---
+++ leetcode/invert-binary-tree.cpp
@@ -0,0 +1,14 @@
+class Solution {
+	public:
+		TreeNode* invertTree(TreeNode* root) {
+			if (root==NULL)
+				return NULL;
+			TreeNode *temp;
+			temp = root->left;
+			root->left = root->right;
+			root->right = temp;
+			root->left = invertTree(root->left);
+			root->right = invertTree(root->right);
+			return root;
+		}
+};
